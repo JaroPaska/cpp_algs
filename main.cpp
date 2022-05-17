7da791bc750cd98e7b37ce7d9ae75ffb9f26939e
Jaroslav Paska <jarko.paska@gmail.com>
2022-05-17 21:21:11 +0100

    pbds, bugfixes and timer changes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,42 @@
 
 #include "base.h"
 
-[[nodiscard]] constexpr std::vector<long long> make_powers(int n, long long base, long long modulo) {
+#define F first
+#define S second
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define ROF(i, a, b) for (int i = a - 1; i >= b; i--)
+#define all(x) (x).begin(), (x).end()
+#define PB push_back
+#define POP pop_back
+#define MP make_pair
+#define MT make_tuple
+
+#ifndef NDEBUG
+#define newline std::endl
+#else
+#define newline '\n'
+#endif
+
+using namespace std;
+
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+using vvi = vector<vi>;
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vpll = vector<pll>;
+using vvll = vector<vll>;
+using ld = long double;
+using ull = unsigned long long;
+using vc = vector<char>;
+using vvc = vector<vc>;
+
+template <class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using max_heap = priority_queue<T>;
+
+std::vector<long long> make_powers(int n, long long base, long long modulo) {
     std::vector<long long> powers;
     powers.reserve(n + 1);
     powers.emplace_back(1);
@@ -13,10 +48,16 @@
     return powers;
 }
 
-using namespace std;
-
 int main() {
-    TIMER(main_timer);
-    for (int i = 0; i < 10; i++)
-        cout << rng::get<double>() << endl;
+#ifdef NDEBUG
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#endif
+    cout << setprecision(numeric_limits<long double>::max_digits10) << fixed;
+
+    hash_set<int> s;
+    FOR (i, 0, 20)
+        s.insert(i);
+    LOG(s);
+    cout << timer.get() << endl;
 }
